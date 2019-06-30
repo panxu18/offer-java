391549717c3e1afa4b44309347f142a1f7831650
panxu18 <xp545945@163.com>
2019-06-30 17:36:25 +0800

    StackPushPopOrder

---
+++ offer/src/main/java/com/xup/offer/ch4/StackPushPopOrder.java
@@ -0,0 +1,38 @@
+package com.xup.offer.ch4;
+
+import java.util.Stack;
+
+public class StackPushPopOrder {
+	private Stack<Integer> stack = new Stack<>();
+	
+	public boolean IsPopOrder(int [] pushA,int [] popA) {
+		if (pushA == null || popA == null)
+			return false;
+		int index = 0;
+		for (int e : popA) {
+			/*
+			 * 如果是栈顶元素则出栈
+			 */
+			if (!stack.isEmpty() && e == stack.peek()) {
+				stack.pop();
+				continue;
+			}
+			/*
+			 * 如果不是栈顶元素则继续入栈
+			 */
+			while (index < pushA.length && pushA[index] != e)
+				stack.push(pushA[index++]);
+			if (index >= pushA.length)
+				return false;
+			index += 1;
+		}
+		if (stack.isEmpty())
+			return true;
+		return false;
+    }
+	
+	public static void main (String[] args) {
+		System.out.println(new StackPushPopOrder().IsPopOrder(new int[]{1,2,3,4,5}, new int[]{4,5,3,2,1}));
+	}
+
+}
