c58897be22e214fdbc81e3dafc2217ba5a6f665e
panxu18 <xp545945@163.com>
2019-07-01 13:24:07 +0800

    PathInTree

---
+++ offer/src/main/java/com/xup/offer/ch4/PathInTree.java
@@ -0,0 +1,103 @@
+package com.xup.offer.ch4;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class PathInTree {
+
+
+	public class TreeNode {
+		int val = 0;
+		TreeNode left = null;
+		TreeNode right = null;
+
+		public TreeNode(int val) {
+			this.val = val;
+
+		}
+
+	}
+
+
+	public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+
+		if (root == null)
+			return new ArrayList<ArrayList<Integer>>(0);
+
+		int sum = 0;
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode p = root;
+		/*
+		 * 遍历左节点
+		 */
+		while (p != null) {
+			sum += p.val;
+			stack.push(p);
+			p = p.left;
+		}
+		while (!stack.isEmpty()) {
+			/*
+			 * 遍历右节点
+			 */
+			p = stack.peek();
+			if (p.right != null) {
+				p = p.right;
+				while (p != null) {
+					sum += p.val;
+					stack.push(p);
+					p = p.left;
+				}
+			}
+			/*
+			 * 叶子节点
+			 */
+			p = stack.peek();
+			if (p.left == null && p.right == null) {
+				if (sum == target) {
+					ArrayList<Integer> list = new ArrayList<>(stack.size());
+					for (TreeNode e : stack) {
+						list.add(e.val);
+					}
+					result.add(list);
+					// stream操作可能会导致超时
+					/*result.add((ArrayList<Integer>) stack.stream()
+							.map((node)->node.val)
+							.collect(Collectors.toList()));*/
+				}
+			}
+			/*
+			 * 出栈
+			 */
+			p = stack.pop();
+			sum -= p.val;
+			while (!stack.isEmpty() && stack.peek().right == p) {
+				p = stack.pop();
+				sum -= p.val;
+			}
+		}
+		// stream操作可能会导致超时
+		/*return result.stream()
+				.sorted(Comparator.comparing(ArrayList<Integer>::size).reversed())
+				.collect(Collectors.toList());*/
+		return result;
+	}
+
+	public static void main(String[] args) {
+		PathInTree pathInTree = new PathInTree();
+		TreeNode root = pathInTree.new TreeNode(10);
+		root.left = pathInTree.new TreeNode(5);
+		root.right = pathInTree.new TreeNode(12);
+		TreeNode p = root.left;
+		p.left = pathInTree.new TreeNode(4);
+		p.right = pathInTree.new TreeNode(7);
+		System.out.println(pathInTree.FindPath(root, 22));
+
+	}
+
+
+
+
+}
