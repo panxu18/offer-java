2f931d7508c782c8d203505492046547cc530dce
panxu18 <xp545945@163.com>
2019-07-04 11:48:56 +0800

    SortArrayForMinNumber

---
+++ offer/src/main/java/com/xup/offer/ch5/SortArrayForMinNumber.java
@@ -0,0 +1,27 @@
+package com.xup.offer.ch5;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SortArrayForMinNumber {
+	/**
+	 * 数字a，b的两种组合ab和ba进行对比，如果ab小于ba，
+	 * 则a要排在前面，通过这种方法对整个数组排序即可
+	 * @param numbers
+	 * @return
+	 */
+	public String sortArray(int [] numbers) {
+		return IntStream.of(numbers)
+				.mapToObj(val->String.valueOf(val))
+				.sorted((a, b)->{
+					String str1 = a + b;
+					String str2 = b + a;
+					return str1.compareTo(str2);
+				})
+				.collect(Collectors.joining());
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new SortArrayForMinNumber().sortArray(new int[]{3,331,2, 23,321,213}));
+	}
+}
