96d0de60f5054a51b8f7d59897e4b81b23b1fd21
panxu18 <xp545945@163.com>
2019-07-14 16:01:19 +0800

    modify ConstructArray

--- offer/src/main/java/com/xup/offer/ch6/ConstructArray.java
+++ offer/src/main/java/com/xup/offer/ch6/ConstructArray.java
@@ -2,8 +2,6 @@ package com.xup.offer.ch6;
 
 import java.util.stream.IntStream;
 
-import com.sun.media.jfxmedia.events.NewFrameEvent;
-
 public class ConstructArray {
 	/**
 	 * 构建数组保存乘积
@@ -14,23 +12,16 @@ public class ConstructArray {
 		if (A == null || A.length <= 1) {
 			return new int[0];
 		}
-		//0~n-2
-		int[] productZero2I = new int[A.length - 1];
-		//1~n-1
-		int[] productI2N = new int[A.length - 1];
-		productZero2I[0] = A[0];
-		for (int i = 1; i < A.length - 1; i++) {
-			productZero2I[i] = A[i] * productZero2I[i - 1];
-		}
-		productI2N[A.length - 2] = A[A.length - 1];
-		for (int i = A.length - 3; i >= 0; i--) {
+		int[] productI2N = new int[A.length];
+		productI2N[A.length - 1] = 1;
+		for (int i = A.length - 2; i >= 0; i--) {
 			productI2N[i] = productI2N[i + 1] * A[i + 1];
 		}
 		int[] B = new int[A.length];
-		B[0] = productI2N[0];
-		B[B.length - 1] = productZero2I[A.length - 2];
-		for (int i = 1; i < B.length - 1; i++) {
-			B[i] = productZero2I[i - 1] * productI2N[i];
+		int product = 1;
+		for (int i = 0; i < B.length; i++) {
+			B[i] = product *  productI2N[i];
+			product *= A[i];
 		}
 		return B;
 	}
